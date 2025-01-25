48b747fccca45e31530aa3c04b02c7ba121713c7
abdo-spices <spices.digital@gmail.com>
2025-01-25 06:58:45 +0200

    init 🚀

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Logosvg from '/icon.svg';
+import './App.css';
+import { Toaster, toast } from 'sonner'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+      <Toaster closeButton  />
+
+        <a href="https://wxt.dev" target="_blank">
+          <img src={Logosvg} className="logo" alt="WXT logo" />
+        </a>
+      </div>
+      <div className="card">
+      <button onClick={() => toast('My first toast')}>
+        Give me a toast
+      </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
